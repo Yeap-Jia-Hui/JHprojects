45d24170a04a7f0929e07df102c161b77f6fe337
yeapjiahui1032-glitch <yeapjiahui1032@gmail.com>
2026-04-12 21:04:31 +0800

    Fix import path for TextLoader and update requirements for langchain-core and python-dotenv

--- RAGChatbot/Storagerouting.py
+++ RAGChatbot/Storagerouting.py
@@ -1,6 +1,6 @@
 import os
 import glob
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
